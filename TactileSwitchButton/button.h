9b1fc1f06f8ff96a3aa729ec939e86ac937baa4f
alex\Александр <alexandervelilyaev@gmail.com>
2018-12-23 01:11:33 +0300

    upload library

---
+++ TactileSwitchButton/button.h
@@ -0,0 +1,29 @@
+﻿/*
+* button.h
+*
+* Created: 23.12.2018 0:48:00
+* Author : Alexander Velilyaev
+*/
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define PORTx PORTB
+#define DDRx DDRB
+#define PINx PINB
+
+typedef struct {
+	uint8_t IsClicked;//is clicked state
+	uint8_t Pin;//number of pin
+	void (*OnButtonDown)(void);//on buttonDown event handler
+	void (*OnButtonUp)(void);//on buttonUp event handler
+} Button;
+
+Button CreateButton(uint8_t pin, void (*onBtnDown)() ,void (*onBtnUp)());
+
+void Poll(Button* button);
+
+#endif /* BUTTON_H_ */
---
+++ TactileSwitchButton/library.c
@@ -0,0 +1,42 @@
+/*
+* TactileSwitchButton.c
+*
+* Created: 23.12.2018 0:48:00
+* Author : Alexander Velilyaev
+*/
+
+#include "button.h"
+#define SET_BIT(DST, POS, VALUE) DST = (DST & ~(1 << POS)) | (VALUE << POS)
+#define GET_BIT(SRC, POS) (SRC & (1 << POS)) >> POS
+
+Button CreateButton(uint8_t pin, void (*onBtnDown)() ,void (*onBtnUp)())
+{
+	Button button;
+	button.Pin = pin;
+	button.IsClicked = 0;
+	button.OnButtonDown = onBtnDown;
+	button.OnButtonUp = onBtnUp;
+
+	//set button's pin in read mode
+	SET_BIT(DDRx, (button.Pin), 0);
+
+	//pull-up resistor
+	SET_BIT(PORTx, (button.Pin), 1);
+
+	return button;
+}
+
+void Poll(Button* button)
+{
+	_delay_ms(10);
+	if (~(GET_BIT(PINx, (button->Pin))) && !button->IsClicked)
+	{
+		button->IsClicked = 1;
+		button->OnButtonDown();
+	}
+	if (GET_BIT(PINx, (button->Pin)) && button->IsClicked)
+	{
+		button->IsClicked = 0;
+		button->OnButtonUp();
+	}
+}
